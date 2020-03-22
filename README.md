d348eaca5ef849db4f425705b510570968b0f06f
BlackGoku36 <urjasvisuthar@gmail.com>
2020-03-22 23:46:22 +0530

    Start Kha

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # BG36-tutorials
 
 Tutorial for Armory3D, Iron and Kha.
+
+Using showdown:
+```bash
+showdown makehtml -i docs/Game_Demo/City_Sim/index.md -o export.html --tables
+```
---
+++ docs-html/Kha/Getting_Started/sidebar.js
@@ -0,0 +1,19 @@
+document.write(`
+<h3 class="sidebar-h"><a href="../../index.html">BG36-Tutorials</a></h3>
+<p class="sidebar-sub">Kha</p>
+<hr>
+<button class="dropdown-btn">Get Started
+    <i class="fa fa-caret-down"></i>
+</button>
+<div class="active dropdown-container">
+    <a href="introduction.html">Introduction</a>
+    <a href="setup.html">Setup</a>
+</div>
+
+<button class="dropdown-btn">Other
+    <i class="fa fa-caret-down"></i>
+</button>
+<div class="active dropdown-container">
+    <a href="../../FAQ.html">FAQ</a>
+</div>
+`);
